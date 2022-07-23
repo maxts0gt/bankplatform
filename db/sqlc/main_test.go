e440ab3c241b04ab0af99be0d03bec5277d681dc
Max Tsogt <bazarsadtsogtjargal@gmail.com>
2022-07-23 18:57:16 +0900

    feat: add CRUD entry and transfer

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,30 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgres://root:secret@localhost:5433/postgres?sslmode=disable"
+)
+
+var testQueries *Queries
+
+func TestMain(m *testing.M) {
+	conn, err := sql.Open(dbDriver, dbSource)
+
+	if err != nil {
+		log.Fatal("cannot connect to the db:", err)
+	}
+
+	testQueries = New(conn)
+
+	os.Exit(m.Run())
+
+}
